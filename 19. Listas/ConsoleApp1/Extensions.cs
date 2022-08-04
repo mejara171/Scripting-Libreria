16aaa9043ee5de64d6a6be72566c3f01d1f438b4
Juanpamoga <martin.zuluaga@upb.edu.co>
2022-08-04 16:47:02 -0500

    1. Agregando archivos

---
+++ 19. Listas/ConsoleApp1/Extensions.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays
+{
+    public static class Extensions
+    {
+        //metodo de extension. Se le agrega al array una funcion llamada PrintElements.
+        public static void PrintElements<T>(this T[] array)
+        {
+            if (null == array || array.Length == 0)
+            {
+                Console.WriteLine("[]");
+                return;
+            }
+
+            Console.Write("[");
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i == 0)
+                {
+                    Console.Write($"{array[i]}");
+                }
+                else
+                {
+                    Console.Write($", {array[i]}");
+                }
+            }
+            Console.WriteLine("]");
+        }
+
+        public static void PrintElements<T>(this List<T> arrayList)
+        {
+            if (null == arrayList || arrayList.Count == 0)
+            {
+                Console.WriteLine("[]");
+                return;
+            }
+
+            Console.Write("[");
+            for (int i = 0; i < arrayList.Count; i++)
+            {
+                if (i == 0)
+                {
+                    Console.Write($"{arrayList[i]}");
+                }
+                else
+                {
+                    Console.Write($", {arrayList[i]}");
+                }
+            }
+            Console.WriteLine("]");
+        }
+
+        public static void PrintElements<T>(this ArrayList<T> arrayList)
+        {
+            if (null == arrayList || arrayList.Length == 0)
+            {
+                Console.WriteLine("[]");
+                return;
+            }
+
+            Console.Write("[");
+            for (int i = 0; i < arrayList.Length; i++)
+            {
+                if (i == 0)
+                {
+                    Console.Write($"{arrayList[i]}");
+                }
+                else
+                {
+                    Console.Write($", {arrayList[i]}");
+                }
+            }
+            Console.WriteLine($"], storageSize: {arrayList.StorageLength}");
+        }
+    }
+}
