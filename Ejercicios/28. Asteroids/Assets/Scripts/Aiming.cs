f55f5cfe08483749373f016ed04c3fbb6f41dd45
Martin <martin.zuluaga@upb.edu.co>
2022-08-23 17:20:02 -0500

    4.1 Actualizacion de librerias y agregar ejercicio

---
+++ Ejercicios/28. Asteroids/Assets/Scripts/Aiming.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Asteroids
+{
+    public class Aiming : MonoBehaviour
+    {
+        private void Update()
+        {
+            Vector3 direction = GetMouseAiming();
+			transform.rotation = Quaternion.LookRotation (direction);
+        }
+
+		private void OnDrawGizmos ()
+		{
+			Debug.DrawRay (transform.position, transform.forward);
+		}
+
+        private Vector3 GetMouseAiming()
+        {
+			Vector3 mouse = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			mouse.z = transform.position.z;
+            Vector3 mouseDirection = (mouse - transform.position).normalized;
+			return mouseDirection;
+        }
+    }
+}
---
+++ Ejercicios/28. Asteroids/Assets/Scripts/Movement.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+namespace Asteroids
+{
+    public class Movement : MonoBehaviour
+    {
+        [SerializeField]
+        private float speed;
+
+        [SerializeField]
+        private float lerpTime = 4f;
+
+        private bool lerping;
+        private Vector3 goalPosition;
+        private Vector3 startPosition;
+        private float elapsedTime;
+
+        private void Update()
+        {
+            float movement = Input.GetAxis("Vertical");
+            if (!Mathf.Approximately(movement, 0))
+            {
+                transform.Translate(Vector3.forward * speed * movement * Time.deltaTime);
+                StopLerping();
+            }
+            else if (Input.GetMouseButtonDown(0))
+                StartLerp();
+
+            if (lerping)
+                UpdatePosition();
+        }
+
+        private void StartLerp()
+        {
+            lerping = true;
+            startPosition = transform.position;
+            elapsedTime = 0;
+            Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mouse.z = transform.position.z;
+            goalPosition = mouse;
+        }
+
+        private void UpdatePosition()
+        {
+            elapsedTime += Time.deltaTime;
+            Debug.Log(elapsedTime / lerpTime);
+            transform.position = Vector3.Lerp(startPosition, goalPosition, elapsedTime / lerpTime);
+            if (elapsedTime / lerpTime >= 1f)
+                StopLerping();
+        }
+
+        private void StopLerping()
+        {
+            Debug.Log("Stop Lerping");
+            lerping = false;
+        }
+    }
+}
