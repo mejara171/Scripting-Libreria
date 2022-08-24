f96bbbd535b1d0162de3222688e099828defe325
Martin Zuluaga <martin.zuluaga@upb.edu.co>
2022-08-23 19:27:40 -0500

    4.3 Disparo funcional

--- Ejercicios/28. Asteroids/Assets/Scripts/Aiming.cs
+++ Ejercicios/28. Asteroids/Assets/Scripts/Aiming.cs
@@ -5,10 +5,22 @@ namespace Asteroids
 {
     public class Aiming : MonoBehaviour
     {
+        public Rigidbody2D bullet;
+        public float BulletSpeed = 8f;
+
         private void Update()
         {
             Vector3 direction = GetMouseAiming();
 			transform.rotation = Quaternion.LookRotation (direction);
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+
+                var BuletInst = Instantiate(bullet, transform.position, Quaternion.Euler(new Vector3(0, 0,0)));
+                BuletInst.velocity = new Vector3(direction.x, direction.y,direction.z)*BulletSpeed;
+                
+
+            }
         }
 
 		private void OnDrawGizmos ()
@@ -16,12 +28,13 @@ namespace Asteroids
 			Debug.DrawRay (transform.position, transform.forward);
 		}
 
-        private Vector3 GetMouseAiming()
+        public Vector3 GetMouseAiming()
         {
 			Vector3 mouse = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 			mouse.z = transform.position.z;
             Vector3 mouseDirection = (mouse - transform.position).normalized;
 			return mouseDirection;
         }
+
     }
 }
